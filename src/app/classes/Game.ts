458445614105d3fc848b4fd1efe176adb084d18e
Jonathan Bluhm <bluhmjc@gmail.com>
2025-02-23 17:43:42 -0600

    Add game and player classes

---
+++ src/app/classes/Game.ts
@@ -0,0 +1,32 @@
+import { Deck } from "./Deck";
+import { Player } from "./Player";
+
+export class Game {
+    players: Player[];
+    deck: Deck;
+
+    constructor(playerNames: string[]) {
+        this.players = playerNames.map(name => new Player(name));
+        this.deck = new Deck();
+        this.deck.shuffle();
+    }
+
+    start() {
+        this.dealCards();
+    }
+
+    dealCards() {
+        const numPlayers = this.players.length;
+        const numCards = this.deck.cards.length;
+
+        for (let i = 0; i < numCards; i++) {
+            const player = this.players[i % numPlayers];
+            player.addCard(this.deck.cards[i]);
+        }
+    }
+
+    determineWinner(): Player | null {
+        // Implement logic to determine the winner
+        return null;
+    }
+}
---
+++ src/app/classes/Player.ts
@@ -0,0 +1,24 @@
+import { Card } from "./Card";
+
+export class Player {
+    name: string;
+    hand: Card[];
+
+    constructor(name: string) {
+        this.name = name;
+        this.hand = [];
+    }
+
+    addCard(card: Card) {
+        this.hand.push(card);
+    }
+
+    removeCard(card: Card): boolean {
+        const index = this.hand.indexOf(card);
+        if (index > -1) {
+            this.hand.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+}
