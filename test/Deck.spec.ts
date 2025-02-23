6c99bd82889168dac9664a092da65e136b097bff
Jonathan Bluhm <bluhmjc@gmail.com>
2025-02-23 17:37:30 -0600

    Add not owrking test

---
+++ test/Deck.spec.ts
@@ -0,0 +1,33 @@
+import { Deck } from "../src/app/classes/Deck";
+import { Card } from "../src/app/classes/Card";
+import { Suit } from "../src/app/enums/Suits";
+import { Face } from "../src/app/enums/Face";
+
+describe('Deck', () => {
+    let deck: Deck;
+
+    beforeEach(() => {
+        deck = new Deck();
+    });
+
+    test('should initialize with 52 cards', () => {
+        expect(deck.cards.length).toBe(52);
+    });
+
+    test('should contain all suits and faces', () => {
+        const suits = [Suit.Hearts, Suit.Diamonds, Suit.Clubs, Suit.Spades];
+        const faces = [Face.Two, Face.Three, Face.Four, Face.Five, Face.Six, Face.Seven, Face.Eight, Face.Nine, Face.Ten, Face.Jack, Face.Queen, Face.King, Face.Ace];
+
+        for (const suit of suits) {
+            for (const face of faces) {
+                expect(deck.cards).toContainEqual(new Card(suit, face));
+            }
+        }
+    });
+
+    test('should shuffle the deck', () => {
+        const originalOrder = [...deck.cards];
+        deck.shuffle();
+        expect(deck.cards).not.toEqual(originalOrder);
+    });
+});
