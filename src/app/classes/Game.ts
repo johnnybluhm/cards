6912f7cd5134419c70045aa822e1b2a4b9ef0cb4
Jonathan Bluhm <bluhmjc@gmail.com>
2025-02-23 18:14:13 -0600

    Add round to game

--- src/app/classes/Game.ts
+++ src/app/classes/Game.ts
@@ -1,30 +1,24 @@
 import { Deck } from "./Deck";
 import { Player } from "./Player";
+import { Round } from "./Round";
 
 export class Game {
     playerToStartId: string 
     deck: Deck;
     players: Player[];
+    round: Round
 
     constructor(playerNames: string[]) {
         this.players = playerNames.map(name => new Player(name));
         this.deck = new Deck();
         this.deck.shuffle();
         this.playerToStartId = this.players[0].id; // Set the first player to start
+        this.round = new Round(playerNames);
     }
 
-    start() {
-        this.dealCards();
-    }
-
-    dealCards() {
-        const numPlayers = this.players.length;
-        const numCards = this.deck.cards.length;
-        this.deck.shuffle(); // Shuffle the deck before dealing
-        for (let i = 0; i < numCards; i++) {
-            const player = this.players[i % numPlayers];
-            player.addCard(this.deck.cards[i]);
-        }
+    beginNewRound() {
+        this.round = new Round(this.players.map(player => player.name));
+        this.round.start();
     }
 
     determineWinner(): Player | null {
