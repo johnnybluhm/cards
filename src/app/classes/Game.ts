9017d261dd1925d3652ba9295151fe4c4fbad6c5
Jonathan Bluhm <bluhmjc@gmail.com>
2025-02-23 18:06:37 -0600

    Shuffle deck before dealing

--- src/app/classes/Game.ts
+++ src/app/classes/Game.ts
@@ -18,7 +18,7 @@ export class Game {
     dealCards() {
         const numPlayers = this.players.length;
         const numCards = this.deck.cards.length;
-
+        this.deck.shuffle(); // Shuffle the deck before dealing
         for (let i = 0; i < numCards; i++) {
             const player = this.players[i % numPlayers];
             player.addCard(this.deck.cards[i]);
