7286f2dd74646bbf7b4f9fce668229308ec05f3f
Jonathan Bluhm <bluhmjc@gmail.com>
2025-02-25 00:29:24 -0600

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,38 @@
 'use client';
 //https://www.pedroalonso.net/blog/websockets-nextjs-part-1/
-import { useEffect } from 'react';
-import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
 import { Events, EventsCallbacks } from './events/Events';
+import { DefaultEventsMap } from 'socket.io';
 
 export default function Home() {
+  const [socket, setSocket] = useState(io());
   useEffect(() => {
-    const socket = io();
-
     socket.on('connect', () => {
       console.log('Connected to server');
     });
     socket.on(Events.message, EventsCallbacks.message);
-
+    setSocket(socket);
     return () => {
       socket.disconnect();
     };
   }, []);
 
+  function joinRoom() {
+    if (socket) {
+      socket.emit(Events.joinRoom, 'room1');
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">WebSocket Test</h1>
-      <button onClick={}></button>
+      <button 
+        onClick={joinRoom}
+        className="bg-blue-500 text-white font-bold py-4 px-8 rounded-lg mt-4"
+      >
+        Join Room
+      </button>
     </main>
   );
 }
