ffcd253085a766717fc4dbcaf8da6a7475e4f1ce
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-08 16:49:10 -0600

    Create view model for card face and suit

--- src/app/components/CardComponent.tsx
+++ src/app/components/CardComponent.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../styles/card.css';
 import { Card as CardModel } from '../classes/Card';
-
+import { Face } from '../enums/Face';
+import { Suit } from '../enums/Suits';
 type Props = {
     card: CardModel;
 }
@@ -9,7 +10,30 @@ type Props = {
 export default function Card({ card }: Readonly<Props>) {
     return (
         <div className="card">
-            {card.face} {card.suit}
+            {FaceString[card.face]} {SuitString[card.suit]}
         </div>
     );
+};
+
+const FaceString = {
+    [Face.Ace]: "Ace",
+    [Face.Two]: "Two",
+    [Face.Three]: "Three",
+    [Face.Four]: "Four",
+    [Face.Five]: "Five",
+    [Face.Six]: "Six",
+    [Face.Seven]: "Seven",
+    [Face.Eight]: "Eight",
+    [Face.Nine]: "Nine",
+    [Face.Ten]: "Ten",
+    [Face.Jack]: "Jack",
+    [Face.Queen]: "Queen",
+    [Face.King]: "King"
+};
+
+const SuitString = {
+    [Suit.Hearts]: "♡",
+    [Suit.Diamonds]: "♢",
+    [Suit.Clubs]: "♣",
+    [Suit.Spades]: "♠"
 };
