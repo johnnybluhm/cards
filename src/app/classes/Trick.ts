701660f419b1d06e2b4df09f280d03bdcab5b477
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-09 01:26:44 -0600

    Update trick class

--- src/app/classes/Trick.ts
+++ src/app/classes/Trick.ts
@@ -3,28 +3,27 @@ import { Suit } from "../enums/Suits";
 import { Card } from "./Card";
 
 export class Trick {
-    cards: Card[];
+    cards: Card[] = [];
+    points: number = 0;
 
-    constructor(cards: Card[]) {
-        if(cards.length !== 4) {
-            throw new Error("A trick must contain exactly 4 cards.");
-        }
-        this.cards = cards;
+    addCard(card: Card) {
+        this.cards.push(card);
+        this.getPoints();
     }
 
-    getPoints(): number {
+    private getPoints(): void {
         let points = 0;
         for (const card of this.cards) {
-            if(card.suit === Suit.Hearts) {
+            if (card.suit === Suit.Hearts) {
                 points += 1;
             }
-            else if(card.suit === Suit.Spades && card.face === Face.Queen) {
+            else if (card.suit === Suit.Spades && card.face === Face.Queen) {
                 points += 13;
             }
-            else if(card.suit === Suit.Diamonds && card.face === Face.Jack) {
-                points -= 10; 
+            else if (card.suit === Suit.Diamonds && card.face === Face.Jack) {
+                points -= 10;
             }
         }
-        return points;
+        this.points = points;
     }
 }
