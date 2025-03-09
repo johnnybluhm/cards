20d2d0fd93fb2c480d63b7343233c7e0c647ac4d
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-08 22:35:30 -0600

    Get cards almost working

--- src/app/components/CardComponent.tsx
+++ src/app/components/CardComponent.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import '../styles/card.css';
+import { Button } from '@mui/material'; // Import div from @mui/material
+import '../card-styles/cards.css';
 import { Card as CardModel } from '../classes/Card';
 import { Face } from '../enums/Face';
 import { Suit } from '../enums/Suits';
-import { Card } from '@mui/material'; // Import div from @mui/material
 
 type Props = {
     card: CardModel;
@@ -12,12 +11,12 @@ type Props = {
 export default function PlayingCard({ card }: Readonly<Props>) {
     console.log(`&${SuitString[card.suit]};`)
     return (
-        <div className="playingCards fourColours simpleCards">
-            <a className={`card rank-${FaceString[card.face].toLocaleLowerCase()} ${SuitString[card.suit]}`} href="#">
+        <div className="playingCards fourColours">
+            <div className={`card rank-${FaceString[card.face].toLocaleLowerCase()} ${SuitString[card.suit]}`} onClick={() => console.log(SuitString[card.suit])}>
                 <span className="rank">{FaceString[card.face]}</span>
-                <span className="suit">{`&${SuitString[card.suit]};`}</span>
-            </a>
-        </div>
+                <span className="suit"></span>
+            </div>
+        </div >
     );
 };
 
@@ -43,6 +42,13 @@ const SuitString = {
     [Suit.Clubs]: "clubs",
     [Suit.Spades]: "spades"
 };
+
+const SuitStringForSpan = {
+    [Suit.Hearts]: "&hearts;",
+    [Suit.Diamonds]: "&diams;",
+    [Suit.Clubs]: "&clubs;",
+    [Suit.Spades]: "&spades;"
+}
 
 const getColor = (suit: Suit): string => {
     if (suit === Suit.Hearts) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 //https://www.pedroalonso.net/blog/websockets-nextjs-part-1/
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import { Button, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
 import { useState } from 'react';
+import './card-styles/cards.css';
 import { Card } from './classes/Card';
 import { Deck } from './classes/Deck';
 import PlayingCard from './components/CardComponent';
-import './card-styles/cards.css';
 const deck = new Deck();
 
 export default function Home() {
@@ -19,40 +19,21 @@ export default function Home() {
     setCards([...deck.cards]);
   }
 
-  const rows = [];
-  for (let i = 0; i < cards.length; i += 10) {
-    rows.push(cards.slice(i, i + 10));
-  }
-
   return (
     <>
+      <Button onClick={shuffleDeck}>Shuffle Deck</Button>
+
       <div className="playingCards">
-        <div className="card rank-7 spades">
-          <span className="rank">7</span>
-          <span className="suit">&spades;</span>
-        </div>
-        <a className="card rank-q hearts" href="#">
-          <span className="rank">Q</span>
-          <span className="suit">&hearts;</span>
-        </a>
+        <ul className="table">
+          {cards.map((card, cardIndex) => (
+            < li key={cardIndex}>
+              <PlayingCard card={card} />
+            </li>
+
+          ))}
+        </ul >
       </div>
 
-      <Button onClick={shuffleDeck}>Shuffle Deck</Button>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableBody>
-            {rows.map((row, rowIndex) => (
-              <TableRow key={rowIndex}>
-                {row.map((card, cardIndex) => (
-                  <TableCell key={cardIndex}>
-                    <PlayingCard card={card} />
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
 
       <Button onClick={shuffleDeck}>Shuffle Deck</Button>
     </>
