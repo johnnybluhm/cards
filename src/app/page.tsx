b2340c81a0c0f1e58afbed49ca58f8f1f7c94ff0
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-08 23:20:14 -0600

    Add basic grid support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import './card-styles/cards.css';
 import { Card } from './classes/Card';
 import { Deck } from './classes/Deck';
 import Hand from './components/HandComponent';
+import Grid from '@mui/material/Grid2';
 const deck = new Deck();
 
 export default function Home() {
@@ -26,19 +27,27 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div className="playingCards fourColours simpleCards">
       <h3>Play Hearts</h3>
       <br />
       <br />
+      <Grid container spacing={2}>
+        <Grid size={8}>
+          <Hand cards={cards.slice(0, 13)} />
+        </Grid>
+        <Grid size={4}>
+          <Hand cards={cards.slice(13, 26)} />
+        </Grid>
+        <Grid size={4}>
+          <Hand cards={cards.slice(26, 39)} />
+        </Grid>
+        <Grid size={8}>
+          <Hand cards={cards.slice(39, 52)} />
+        </Grid>
+      </Grid>
 
-      <div className="playingCards fourColours simpleCards">
-        <Hand cards={cards.slice(0, 13)} />
-        <Hand cards={cards.slice(13, 26)} />
-        <Hand cards={cards.slice(26, 39)} />
-        <Hand cards={cards.slice(39, 52)} />
-        <Button onClick={shuffleDeck}>Shuffle Deck</Button>
-        <Button onClick={sortDeck}>Sort Deck</Button>
-      </div >
-    </>
+      <Button onClick={shuffleDeck}>Shuffle Deck</Button>
+      <Button onClick={sortDeck}>Sort Deck</Button>
+    </div >
   );
 }
