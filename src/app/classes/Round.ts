2ac923339595ff0cc3c5144abd0290de728f5fde
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-11 16:23:16 -0500

    Update round class constructor

--- src/app/classes/Round.ts
+++ src/app/classes/Round.ts
@@ -1,14 +1,10 @@
 import { Card } from "./Card";
-import { Deck } from "./Deck";
-import { Player } from "./Player";
 import { Trick } from "./Trick";
 
 export class Round {
     completedTricks: Trick[];
     currentTrick: Trick;
-    cards: Card[];
-    constructor(cards: Card[]) {
-        this.cards = cards;
+    constructor() {
         this.completedTricks = [];
         this.currentTrick = new Trick();
     }
@@ -25,5 +21,4 @@ export class Round {
         this.completedTricks.push(this.currentTrick);
         this.currentTrick = new Trick();
     }
-}
 }
