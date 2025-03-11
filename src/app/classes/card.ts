d992dbbfe72fd364276af70cb9a04ea089c64601
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-11 16:09:50 -0500

    Add owner prop to card

--- src/app/classes/card.ts
+++ src/app/classes/card.ts
@@ -1,12 +1,22 @@
+import { UUID } from "crypto";
 import { Face } from "../enums/Face";
 import { Suit } from "../enums/Suits";
 
 export class Card {
     suit: Suit;
     face: Face;
+    ownerId?: UUID;
 
     constructor(suit: Suit, face: Face) {
         this.suit = suit;
         this.face = face;
+    }
+
+    addOwner(ownerId: UUID) {
+        this.ownerId = ownerId;
+    }
+
+    removeOwner() {
+        this.ownerId = undefined;
     }
 }
