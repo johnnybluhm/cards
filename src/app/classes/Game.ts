cbaa27dde71a24023d23f731fb102f06808c3579
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-11 16:47:08 -0500

    Wrap up game logic

--- src/app/classes/Game.ts
+++ src/app/classes/Game.ts
@@ -4,31 +4,32 @@ import { Player } from "./Player";
 import { Round } from "./Round";
 
 export default class Game {
-    playerToStartId: string
+    playerToStartIndex: number
     deck: Deck;
     players: Player[];
     round: Round
-
 
     constructor(playerNames: string[]) {
         this.players = playerNames.map(name => new Player(name));
         this.deck = new Deck();
         this.deck.shuffle();
-        this.playerToStartId = this.players[0].id; // Set the first player to start
-        this.round = new Round(playerNames);
+        this.playerToStartIndex = Math.floor(Math.random() * this.players.length);
+        this.round = new Round();
     }
 
     beginNewRound() {
         this.deck = new Deck();
         this.deck.shuffle();
         this.dealCards();
-        this.round = new Round(this.deck.cards);
-        this.round.start();
+        this.round = new Round();
     }
 
     addTrickToWinningPlayer(): void {
         const winningCard = this.round.currentTrick.getWinningCard();
-        this.players.find(player => player.id === winningCard.ownerId)!.addTrickWon(this.round.currentTrick);
+        const winningPlayer = this.players.find(player => player.id === winningCard.ownerId)!;
+        winningPlayer.addTrickWon(this.round.currentTrick);
+        winningPlayer.updatePoints();
+        winningPlayer.isTurn = true;
     }
 
     updateGame(card: Card, playerId: string) {
@@ -41,8 +42,10 @@ export default class Game {
         if (this.round.isTrickComplete()) {
             this.addTrickToWinningPlayer();
             this.round.moveToNextTrick();
-            player?.isTurn
         }
+        player!.isTurn = false;
+        const nextPlayerIndex = (this.players.indexOf(player!) + 1) % this.players.length;
+        this.players[nextPlayerIndex].isTurn = true;
     }
 
     dealCards() {
