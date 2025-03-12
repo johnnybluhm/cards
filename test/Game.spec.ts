bb5f4992abefe54abef11689997ab5ead6c66bcd
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-11 22:39:04 -0500

    Add test for playing card when you have the suit

--- test/Game.spec.ts
+++ test/Game.spec.ts
@@ -1,5 +1,6 @@
 import { Card } from "../src/app/classes/Card";
 import Game from "../src/app/classes/Game";
+import HttpError from "../src/app/classes/HttpError";
 import { Face } from "../src/app/enums/Face";
 import { Suit } from "../src/app/enums/Suits";
 
@@ -65,24 +66,31 @@ describe('Game', () => {
 
         game.players[0].hand = [
             new Card(Face.Two, Suit.Clubs, game.players[0].id),
+            new Card(Face.Jack, Suit.Hearts, game.players[1].id),
         ]
+        //player 1 cannot play A of hearts because he has jack of clubs
         game.players[1].hand = [
             new Card(Face.Ace, Suit.Hearts, game.players[1].id),
+            new Card(Face.Jack, Suit.Clubs, game.players[1].id),
         ]
-        // Player 2 should win the trick
         game.players[2].hand = [
             new Card(Face.King, Suit.Clubs, game.players[2].id),
+            new Card(Face.King, Suit.Diamonds, game.players[2].id),
         ]
         game.players[3].hand = [
             new Card(Face.Queen, Suit.Spades, game.players[3].id),
+            new Card(Face.King, Suit.Spades, game.players[3].id),
         ]
         game.players.forEach(player => player.isTurn = false);
         game.players[0].isTurn = true;
-        for (let i = 0; i < 4; i++) {
-            game.updateGame(game.players[i].hand[0], game.players[i].id);
+        game.updateGame(game.players[0].hand[0], game.players[0].id);
+        try {
+            game.updateGame(game.players[1].hand[0], game.players[1].id);
+            throw new Error("Player 1 should not be able to play Ace of Hearts");
         }
-        expect(game.players[2].totalPoints).toBe(14);
-        expect(game.players[2].tricksWon.length).toBe(1);
-        expect(game.players[2].isTurn).toBe(true);
+        catch (e) {
+            const error = e as HttpError;
+            expect(error.message).toBe("You must follow the trick suit");
+        }
     });
 });
