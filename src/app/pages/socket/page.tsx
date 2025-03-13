2c97e99e52c0ab251cd25eccca5dd2c1087cb836
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-13 18:43:30 -0500

    Add room code to page

--- src/app/pages/socket/page.tsx
+++ src/app/pages/socket/page.tsx
@@ -1,11 +1,33 @@
 'use client';
 //https://www.pedroalonso.net/blog/websockets-nextjs-part-1/
+import { Button, TextField } from '@mui/material';
+import { useSocket } from '../../hooks/useSocket';
+
 export default function Socket() {
+    const { room, joinRoom, messages, sendMessage } = useSocket();
+
+    function SendMessage(event: React.ChangeEvent<HTMLInputElement>) {
+        sendMessage(event.target.value);
+    }
 
     return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">WebSocket Test</h1>
+            <Button
+                onClick={joinRoom}
+            >Join Room</Button>
 
-        <p>You are in room TEst</p>
+            <p>You are in room {room}</p>
 
-    )
+            <TextField onChange={SendMessage} label="Name" variant="outlined">
 
+            </TextField>
+            Messages:
+            <div>
+                {messages.map((message, index) => (
+                    <p key={index}>{message}</p>
+                ))}
+            </div>
+        </main >
+    );
 }
