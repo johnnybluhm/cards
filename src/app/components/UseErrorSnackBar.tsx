30f8bb248d930de5f927f76115f9b4de1595d712
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-13 19:46:46 -0500

    Add useErrorSnackbar

---
+++ src/app/components/UseErrorSnackBar.tsx
@@ -0,0 +1,38 @@
+import { Alert } from '@mui/material';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
+import * as React from 'react';
+import { useCallback, useState } from 'react';
+
+export default function useErrorSnackbar() {
+    const [error, setError] = useState<string | null>(null);
+
+    function handleClose(event: React.SyntheticEvent<Element, Event> | Event, reason: SnackbarCloseReason) {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setError(null);
+    }
+
+    const ErrorSnackBar = useCallback(() => {
+        return (
+            <>
+                {error && <Snackbar
+                    sx={{ minWidth: 400 }}
+                    open={!!error}
+                    onClose={handleClose}
+                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+                    message={error}
+                >
+                    <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+                        {error}
+                    </Alert>
+                </Snackbar>}
+            </>
+        )
+    }, [error]);
+
+    return {
+        setError,
+        ErrorSnackBar
+    }
+}
