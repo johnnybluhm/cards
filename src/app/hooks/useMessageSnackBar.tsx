b94ad6773c740295c4ee43bf471fbfa9ed764200
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-15 17:35:17 -0500

    Add new useMessageSnackBar

---
+++ src/app/hooks/useMessageSnackBar.tsx
@@ -0,0 +1,40 @@
+import { Alert } from '@mui/material';
+import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
+import * as React from 'react';
+import { useCallback, useState } from 'react';
+import Message from '../classes/Message';
+
+export default function useMessageSnackbar() {
+    const [message, setMessage] = useState<Message | null>(null);
+
+    function handleClose(event: React.SyntheticEvent<Element, Event> | Event, reason?: SnackbarCloseReason) {
+        setMessage(null);
+    }
+
+    const ErrorSnackBar = useCallback(() => {
+        return (
+            <>
+                {message && <Snackbar
+                    sx={{ minWidth: 400 }}
+                    open={!!message}
+                    onClose={handleClose}
+                    autoHideDuration={5000}
+                >
+                    <Alert
+                        onClose={handleClose}
+                        severity={message.severity}
+                        variant="filled"
+                        sx={{ width: '100%' }}
+                    >
+                        {message.content}
+                    </Alert>
+                </Snackbar>}
+            </>
+        )
+    }, [message]);
+
+    return {
+        setMessage,
+        ErrorSnackBar
+    }
+}
