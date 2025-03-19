a807765a5eb5b966b521e8f01cb03d40c78a50b2
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-19 15:54:57 -0500

    Change default port back to 3000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
   useEffect(() => {
     getRooms();
-    const playerName = 'Player' + socketId
+    //const playerName = 'Player' + socketId
     //createRoom('test', 'test', playerName);
     //joinRoom('test', 'test', playerName);
   }, []);
