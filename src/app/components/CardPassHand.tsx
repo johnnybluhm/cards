2822a31bc7b9e87dfed8e59b5de78e21abd6fad5
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-20 16:37:25 -0500

    Remove arrow icon when passing

--- src/app/components/CardPassHand.tsx
+++ src/app/components/CardPassHand.tsx
@@ -1,18 +1,15 @@
 import { Button } from '@mui/material';
+import { useState } from 'react';
 import '../card-styles/cards.css';
 import { Card as CardModel } from '../classes/Card';
-import { Player } from '../classes/Player';
-import PassingCard from './PassingCard';
-import { useState } from 'react';
-import { sortHand } from './HandComponent';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { PassType } from '../classes/Game';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Player } from '../classes/Player';
+import { sortHand } from './HandComponent';
+import PassingCard from './PassingCard';
 type Props = {
     player: Player;
     passCards: (cards: CardModel[]) => void;
-    passType: PassType
+    passType?: PassType
 }
 
 export default function CardPassHand({ player, passCards, passType }: Readonly<Props>) {
@@ -23,21 +20,6 @@ export default function CardPassHand({ player, passCards, passType }: Readonly<P
     }
     return (
         <>
-            {passType === PassType.Left && <ArrowBackIcon sx={{
-                color: 'black',
-                fontSize: 200,
-                marginLeft: 18
-            }} onClick={() => { }} />}
-            {passType === PassType.Across && <ArrowUpwardIcon sx={{
-                color: 'black',
-                fontSize: 200,
-                marginLeft: 18
-            }} onClick={() => { }} />}
-            {passType === PassType.Right && <ArrowForwardIcon sx={{
-                color: 'black',
-                fontSize: 200,
-                marginLeft: 18
-            }} onClick={() => { }} />}
             <ul className="hand">
                 {player.hand.map((card, cardIndex) => (
                     <li key={`${cardIndex}-${card.ownerId}`}>
@@ -58,3 +40,19 @@ export default function CardPassHand({ player, passCards, passType }: Readonly<P
         </>
     );
 };
+
+/*{passType === PassType.Left && <ArrowBackIcon sx={{
+                color: 'black',
+                fontSize: 200,
+                marginLeft: 18
+            }} onClick={() => { }} />}
+            {passType === PassType.Across && <ArrowUpwardIcon sx={{
+                color: 'black',
+                fontSize: 200,
+                marginLeft: 18
+            }} onClick={() => { }} />}
+            {passType === PassType.Right && <ArrowForwardIcon sx={{
+                color: 'black',
+                fontSize: 200,
+                marginLeft: 18
+            }} onClick={() => { }} />}*/
