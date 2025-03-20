5bc7d91cc0da6f254763444e395a59a04c24d397
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-19 23:03:50 -0500

    Change io address to be localhost

--- src/app/socket.js
+++ src/app/socket.js
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 //'http://52.15.132.197:3001
-export const clientSocket = io('ws://52.15.132.197:3001');
+export const clientSocket = io('ws://localhost:3001');
