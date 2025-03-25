03e92db1834e2e99c00b3498ac4997ecd144cd47
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-24 21:28:13 -0500

    Improve spacing and add slider

--- src/app/singlePlayer/page.tsx
+++ src/app/singlePlayer/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from "@mui/material";
+import { Box, Button, Slider } from "@mui/material";
 import SinglePlayer from "../components/SinglePlayer";
 import { useRouter } from "next/navigation";
 
@@ -10,16 +10,16 @@ export default function SinglePlayerPage() {
         <>
             <div>
                 <h3> Play Hearts</h3 >
+                <br></br>
                 <Button variant="contained" color="secondary" onClick={() => router.push('/')}>
                     Multi Player
                 </Button>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
+                <br></br>
+                <br></br>
+                <Box sx={{ width: 300 }}>
+                    <h3>Animation speed</h3>
+                    <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+                </Box>
             </div>
             <SinglePlayer />
         </>
