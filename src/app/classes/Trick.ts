94769df9f0fb1a009334a7da7c2fbda3d618e501
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-24 20:12:38 -0500

    Update error message

--- src/app/classes/Trick.ts
+++ src/app/classes/Trick.ts
@@ -13,7 +13,7 @@ export class Trick {
         if (this.cards.length > 0 &&
             card.suit !== this.trickSuit &&
             playerCardsInHand.some(c => c.suit === this.trickSuit)) {
-            throw new Error(`You must follow the trick suit. Trick suit is ${this.trickSuit} and you tried playing ${card.suit}`);
+            throw new Error(`You must follow the trick suit. Trick suit is ${this.trickSuit} and you tried playing ${card.suit}, face ${card.face}`);
         }
         this.cards.push(card);
         this.updatePoints();
