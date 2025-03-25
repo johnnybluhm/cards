637e294fd663573b6d91dafba66c31c11a2c0024
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-24 21:13:28 -0500

    Add multiPlayer toggle

--- src/app/singlePlayer/page.tsx
+++ src/app/singlePlayer/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import { Button } from "@mui/material";
 import SinglePlayer from "../components/SinglePlayer";
+import { useRouter } from "next/navigation";
 
 export default function SinglePlayerPage() {
+    const router = useRouter();
     return (
         <>
             <div>
                 <h3> Play Hearts</h3 >
+                <Button variant="contained" color="secondary" onClick={() => router.push('/')}>
+                    Multi Player
+                </Button>
                 <br />
                 <br />
                 <br />
