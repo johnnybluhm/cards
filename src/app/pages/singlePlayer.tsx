c19e499dd18c1a2b9fb1e528db4d49b09ca23db8
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-24 21:04:12 -0500

    Add singlePlayer page

---
+++ src/app/pages/singlePlayer.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import SinglePlayer from "../components/SinglePlayer";
+
+export default function Home() {
+    return (
+        <>
+            <div>
+                <h3> Play Hearts</h3 >
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+            </div>
+            <SinglePlayer />
+        </>
+    );
+}
