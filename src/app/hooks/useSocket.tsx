f43faa56a76ae6558664298114ae76ceb7209402
Jonathan Bluhm <bluhmjc@gmail.com>
2025-03-26 22:11:28 -0500

    Fix lost connection problem

--- src/app/hooks/useSocket.tsx
+++ src/app/hooks/useSocket.tsx
@@ -13,6 +13,9 @@ export const useSocket = () => {
     const [game, setGame] = useState<Game | null>(null);
     const socketId = socket.id;
     useEffect(() => {
+        if (!socket.id) {
+            socket.connect();
+        }
         socket.on('connect', () => {
             console.log('Connected to server');
         });
@@ -34,42 +37,12 @@ export const useSocket = () => {
             const game = JSON.parse(updatedGame) as Game;
             console.log('Got game', game);
             setGame(game);
-            /*const playerToPlay = game.players.find(player => player.isTurn);
-            if (playerToPlay && (playerToPlay.id === socket.id)) {
-                //auto play logic
-                const deuceOfClubs = playerToPlay.hand.find(card => card.suit === Suit.Clubs && card.face === Face.Two);
-                const nonHeartCard = playerToPlay.hand.find(card => card.suit !== Suit.Hearts);
-                const clubCard = playerToPlay.hand.find(card => card.suit === Suit.Clubs);
-                if (game.round.completedTricks.length === 0) {
-                    if (deuceOfClubs) {
-                        socket.emit(SocketEvent.UpdateGame, deuceOfClubs);
-                    }
-                    else if (clubCard) {
-                        socket.emit(SocketEvent.UpdateGame, clubCard);
-                    }
-                    else if (nonHeartCard) {
-                        socket.emit(SocketEvent.UpdateGame, nonHeartCard);
-                    }
-                    else {
-                        socket.emit(SocketEvent.UpdateGame, playerToPlay.hand[0]);
-                    }
-                    return;
-                }
-                if (game.round.currentTrick.trickSuit) {
-                    const cardOfTrickSuit = playerToPlay.hand.find(card => card.suit === game.round.currentTrick.trickSuit);
-                    console.log('cardOfTrickSuit', cardOfTrickSuit);
-                    socket.emit(SocketEvent.UpdateGame, cardOfTrickSuit ?? playerToPlay.hand[0]);
-                }
-                else {
-                    socket.emit(SocketEvent.UpdateGame, playerToPlay.hand[0]);
-                }
-            }*/
         });
         setSocket(socket);
         return () => {
             socket.disconnect();
         };
-    }, []);
+    });
 
     function joinRoom(roomName: string, password: string, playerName: string) {
         if (socket) {
